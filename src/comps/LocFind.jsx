60b462f9de4de97efb3f46200c0aad3e4ef50435
AnLau25 <avald024@uottawa.ca>
2024-06-06 16:09:05 -0400

    LindFinetune1

--- src/comps/LocFind.jsx
+++ src/comps/LocFind.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import LocCard from './LocCard'
+import './LocFind.css';
 
 const LocFind = () => {
-  return (
-    <section className='LocCard' id='loccard'>
-        <div className='locTitle'>
-            <h1><em>DEUX SITES |<span>Pourquoi nous confier votre voiture ?</span></em></h1>
-            <h1><em>DEUX ÉQUIPES D'EXCELLENCE</em></h1>
-        </div>
-    </section>
-
-  )
+    return (
+        <section className='LocFind' id='LocFind'>
+            <div className='locTitle'>
+                <div className='titleContainer'>
+                    <h1><em>DEUX SITES |</em></h1>
+                    <span>Pourquoi nous confier votre voiture ?</span>
+                </div>
+                <div className='titleContainer'>
+                    <h1><em>DEUX ÉQUIPES</em></h1>
+                    <h1><em>D'EXCELLENCE</em></h1>
+                </div>
+            </div>
+            <div className='Cardslgn'>
+                <LocCard imgprop='./src/assets/LocNepImg.png' loc='Nepean' t1='LunVen    8h00 - 16h00' t2='SamDim 9h00 - 17h00' adr='33 Rue Lamagie, K1K 3F3' />
+                <LocCard imgprop='./src/assets/LocOrlImg.png' loc='Orléans' t1='LunVen    8h00 - 16h00' t2='SamDim 10h00 - 17h00' adr='55 Operator Priv, K1K 5C5' />
+            </div>
+        </section>
+    )
 }
+
+
 
 export default LocFind
