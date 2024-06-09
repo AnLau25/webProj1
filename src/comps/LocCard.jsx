b1f1a628b4f7f569161ccda74b5dc33356ac550f
AnLau25 <avald024@uottawa.ca>
2024-06-09 03:22:34 -0400

    yeye

--- src/comps/LocCard.jsx
+++ src/comps/LocCard.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './LocCard.css'
+import React from 'react';
+import './LocCard.css';
 
 const LocCard = ({ imgprop, loc, t1, t2, adr }) => {
   return (
     <div className='lcard'>
       <img src={imgprop} alt={loc} />
-      <div class='find'>
+      <div className='find'>
         <h1><em>{loc}</em></h1>
         <div className='time'>
           <p>{t1}</p>
           <p>{t2}</p>
         </div>
         <p>{adr}</p>
-        <a class="nav-link" href="#"> <em>Voir plus</em></a>
+        <a className="nav-link" href="#"><em>Voir plus</em></a>
       </div>
     </div>
-  )
-}
-//Edit the serv ones so Imma call it donne
-export default LocCard
+  );
+};
+
+export default LocCard;
