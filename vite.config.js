58fec3d0a6c9720220ac3fc08ac66e0cc1f5faf9
AnLau25 <avald024@uottawa.ca>
2024-06-08 22:21:49 -0400

    T~T

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/webProj1/",
   plugins: [react()],
+  build: {chunkSizeWarningLimit: 1600}
 })
